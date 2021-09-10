e0e0a30a17520fff5c4b0fbf87e8006900fb01a7
黄咏玉 <hylm@me.com>
2021-09-10 20:00:41 +0800

    加入user/message

--- work/work.go
+++ work/work.go
@@ -5,8 +5,12 @@ import (
 	"github.com/faryoo/wework/work/config"
 	"github.com/faryoo/wework/work/context"
 	"github.com/faryoo/wework/work/kf"
+	"github.com/faryoo/wework/work/menu"
 	"github.com/faryoo/wework/work/msgaudit"
 	"github.com/faryoo/wework/work/oauth"
+	"github.com/faryoo/wework/work/server"
+	"github.com/faryoo/wework/work/user"
+	"net/http"
 )
 
 // Work 企业微信
@@ -24,9 +28,32 @@ func NewWork(cfg *config.Config) *Work {
 	return &Work{ctx: ctx}
 }
 
+// SetAccessTokenHandle 自定义access_token获取方式
+func (wk *Work) SetAccessTokenHandle(accessTokenHandle credential.AccessTokenHandle) {
+	wk.ctx.AccessTokenHandle = accessTokenHandle
+}
+
 // GetContext get Context
 func (wk *Work) GetContext() *context.Context {
 	return wk.ctx
+}
+
+// GetMenu 菜单管理接口
+func (wk *Work) GetMenu() *menu.Menu {
+	return menu.NewMenu(wk.ctx)
+}
+
+// GetServer 消息管理：接收事件，被动回复消息管理
+func (wk *Work) GetServer(req *http.Request, writer http.ResponseWriter) *server.Server {
+	srv := server.NewServer(wk.ctx)
+	srv.Request = req
+	srv.Writer = writer
+	return srv
+}
+
+// GetAccessToken 获取access_token
+func (wk *Work) GetAccessToken() (string, error) {
+	return wk.ctx.GetAccessToken()
 }
 
 // GetOauth get oauth
@@ -37,6 +64,10 @@ func (wk *Work) GetOauth() *oauth.Oauth {
 // GetMsgAudit get msgAudit
 func (wk *Work) GetMsgAudit() (*msgaudit.Client, error) {
 	return msgaudit.NewClient(wk.ctx.Config)
+}
+
+func (wk *Work) GetUser() *user.User {
+	return user.NewUser(wk.ctx)
 }
 
 // GetKF get kf
